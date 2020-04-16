698a1131ed32a244d54e7ddfcb8c11df9bf0ad55
Gustavo Seabra <devguga87@gmail.com>
2020-04-16 00:07:48 -0300

    Refatoração do Create, Repository e routes
    Criação das rotas get e post, criação de transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactionsIncome = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    transactionsIncome.reduce((acc, b) => acc + b.value, 0);
+
+    const sumIncome = transactionsIncome.reduce((acc, b) => acc + b.value, 0);
+
+    const transactionsOutcome = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const sumOutcome = transactionsOutcome.reduce((acc, b) => acc + b.value, 0);
+
+    const total = sumIncome - sumOutcome;
+
+    const balance = {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
